56566a90e5ad29fd3ac36065b6166fb687ba8051
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-12 18:29:50 +0900

    REST API의 기본적인 Get, Post, Delete, Patch 구성 완료

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/Movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: string): Movie {
+    return this.moviesService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string) {
+    return this.moviesService.deleteOne(id);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') id: string, @Body() updateData) {
+    return {
+      updateMovie: id,
+      ...updateData,
+    };
+  }
+}
